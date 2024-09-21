dfcd388eaf42e024cc374b9e8da296b396b67c21
gquiroz368 <gaboaqm@gmail.com>
2024-09-21 09:19:45 -0500

    Update Diccionario de Palabras Peruanas.py

--- Diccionario de Palabras Peruanas.py
+++ Diccionario de Palabras Peruanas.py
@@ -1,1 +1,6 @@
-  
+buscador = input("¿Con que palabra tienes duda?")
+diccionario = {"Palta": "Relacionado a algo embarazoso", "Causa": "Amigo o amiga", "Pe": "Abreviación de la palabra 'pues'"}
+if buscador in diccionario:
+    print(diccionario[buscador])
+else:
+    print("Perdone, seguimos buscando el significado a la palabra.")
